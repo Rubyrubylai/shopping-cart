8cecafe6d909b8a3c98a91c2c7b16eb693174b9a
Ruby Li <r844312@gmail.com>
2020-11-17 15:55:35 +0800

    Add page function

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,12 +3,28 @@ const Product = db.Product
 
 const productContoller = {
   getProducts: (req, res) => {
-    Product.findAll({
-      raw: true,
-      nest: true
+    //每頁幾個商品及偏移多少
+    let pageLimit = 8
+    let offset = 0
+    let currentPage = req.query.page
+    if (currentPage) {
+      offset = (currentPage - 1) * pageLimit
+    }
+   
+    Product.findAndCountAll({
+      raw: true, 
+      nest: true,
+      limit: pageLimit,
+      offset: offset
     })
     .then(products => {
-      res.render('products', { products })
+      //頁數
+      let pages = Math.ceil(products.count/pageLimit)
+      let page = Array.from({ length: pages }).map((item, index) => index + 1)
+      let prev = (currentPage = 1) ? currentPage : currentPage - 1
+      let post = (currentPage = pages) ? currentPage : currentPage + 1
+
+      res.render('products', { products: products.rows, page, prev, post })
     })
   },
 

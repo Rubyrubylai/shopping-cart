25a427f0ff9b425f5bca7584d339cd29e5bcc430
Ruby Li <r844312@gmail.com>
2020-11-22 21:04:03 +0800

    POST admin/new prodocut

---
+++ public/image.js
@@ -0,0 +1,33 @@
+const image = document.getElementById('image')
+const file = document.getElementById('file')
+
+// function showImg(this) {
+//   var file = this.files[0]
+//   console.log(file)
+//   var reader = new FileReader()
+//   reader.onloadend = function(e) {
+//     onloadend = e.target.result
+//   }
+//   reader.readAsDataURL(file)
+
+// }
+// file.on('change', function() {
+//   var upload_img = file[0].files[0]
+//   var reader = new FileReader
+//   reader.onload = function(e) {
+//     image.attr('src', e.target.result)
+//   }
+//   reader.readAsDataURL(upload_img)
+// })
+
+file.addEventListener('change', function(e){
+  console.log(this.files)
+  const file = this.files[0];
+  const fr = new FileReader();
+  fr.onload = function (e) {
+    image.src = e.target.result
+    image.classList = "card-custom"
+  };
+  fr.readAsDataURL(file);
+});
+

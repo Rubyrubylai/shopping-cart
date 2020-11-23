e4f7474e118d38004aa988183904b31930d53c99
Ruby Li <r844312@gmail.com>
2020-11-23 13:01:26 +0800

    Add login and register page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,14 @@
 const userController = {
   getAccount: (req, res) => {
     res.render('user/account')
+  },
+
+  loginPage: (req, res) => {
+    res.render('user/login')
+  },
+
+  registerPage: (req, res) => {
+    res.render('user/register')
   }
 }
 
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController')
 
+router.get('/login', userController.loginPage)
+router.get('/register', userController.registerPage)
+
 router.get('/account', userController.getAccount)
 
 module.exports = router

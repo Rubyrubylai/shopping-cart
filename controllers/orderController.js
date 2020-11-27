24700a74205859da5f326a2c07e33d7c288e76e9
Ruby Li <r844312@gmail.com>
2020-11-27 15:28:05 +0800

    User promise to avoid removing cart twice. try nodemailer but still have bug

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -10,68 +10,14 @@ const sort = require('../config/sort')
 const encrypt = require('../config/encrypt')
 const getTradeInfo = require('../config/getTradeInfo')
 
-
-// function genDataChain(TradeInfo) {
-//   let results = [];
-//   for (let kv of Object.entries(TradeInfo)) {
-//       results.push(`${kv[0]}=${kv[1]}`);
-//   }
-//   return results.join("&");
-// }
-
-// function create_mpg_aes_encrypt(TradeInfo) {
-//   let encrypt = crypto.createCipheriv("aes256", HashKey, HashIV);
-//   let enc = encrypt.update(genDataChain(TradeInfo), "utf8", "hex");
-//   return enc + encrypt.final("hex");
-// }
-
-// function create_mpg_aes_decrypt(TradeInfo) {
-//   let decrypt = crypto.createDecipheriv("aes256", HashKey, HashIV);
-//   decrypt.setAutoPadding(false);
-//   let text = decrypt.update(TradeInfo, "hex", "utf8");
-//   let plainText = text + decrypt.final("utf8");
-//   let result = plainText.replace(/[\x00-\x20]+/g, "");
-//   return result;
-// }
-
-// function create_mpg_sha_encrypt(TradeInfo) {
-//   let sha = crypto.createHash("sha256");
-//   let plainText = `HashKey=${HashKey}&${TradeInfo}&HashIV=${HashIV}`
-//   return sha.update(plainText).digest("hex").toUpperCase();
-// }
-
-// function getTradeInfo(Amt, Desc, email){
-//   data = {
-//     'MerchantID': MerchantID, // 商店代號
-//     'RespondType': 'JSON', // 回傳格式
-//     'TimeStamp': Date.now(), // 時間戳記
-//     'Version': 1.5, // 串接程式版本
-//     'MerchantOrderNo': Date.now(), // 商店訂單編號
-//     'LoginType': 0, // 智付通會員
-//     'OrderComment': 'OrderComment', // 商店備註
-//     'Amt': Amt, // 訂單金額
-//     'ItemDesc': Desc, // 產品名稱
-//     'Email': email, // 付款人電子信箱
-//     'ReturnURL': ReturnURL, // 支付完成返回商店網址
-//     'NotifyURL': NotifyURL, // 支付通知網址/每期授權結果通知
-//     'ClientBackURL': ClientBackURL, // 支付取消返回商店網址
-//   }
-
-//   mpg_aes_encrypt = create_mpg_aes_encrypt(data)
-//   mpg_sha_encrypt = create_mpg_sha_encrypt(mpg_aes_encrypt)
-
-//   tradeInfo = {
-//     'MerchantID': MerchantID, // 商店代號
-//     'TradeInfo': mpg_aes_encrypt, // 加密後參數
-//     'TradeSha': mpg_sha_encrypt,
-//     'Version': 1.5, // 串接程式版本
-//     'PayGateWay': PayGateWay,
-//     'MerchantOrderNo': data.MerchantOrderNo,
-//   }
-
-//   return tradeInfo
-// }
-
+const nodemailer = require('nodemailer')
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'r844312@gmail.com',
+    pass: '',
+  },
+})
 
 const orderController = {
   getOrders: (req, res) => {
@@ -162,18 +108,50 @@ const orderController = {
           UserId: req.user.id
         })
         .then(order => {
+          console.log('--------------------Order')
+          // const items = []
+          // items.push(orderItems.dataValues)
+          const items = cart.toJSON().items
+          let text = ''
+          items.forEach(i => {
+            text += `
+            ${i.name}
+            <img src="${i.image}">
+            $${i.price}
+            `
+          })
+          console.log(text)
+          var mailOptions = {
+            from: 'r844312@gmail.com',
+            to: 'r844312@gmail.com',
+            subject: `Order Confirmation: SHOP #${order.id}`,
+            text: 'Thank you for ordering.'
+          }
+
+          transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+              console.log(error)
+            } else {
+              console.log('Email sent: ' + info.response)
+            }
+          })
+
+          var results = []
           cart.dataValues.items.forEach(items => {
-            OrderItem.create({
-              price: items.dataValues.price,
-              quantity: items.dataValues.CartItem.quantity,
-              ProductId: items.dataValues.id,
-              OrderId: order.id
-            })
-            .then(orderItem => {
-              cart.destroy().then(cart => {
-                return res.redirect(`/order/${order.id}`)
+            results.push(
+              OrderItem.create({
+                price: items.dataValues.price,
+                quantity: items.dataValues.CartItem.quantity,
+                ProductId: items.dataValues.id,
+                OrderId: order.id
               })
-              
+            )
+          })
+
+          return Promise.all(results).then(() => {
+            //訂單成立後，清空購物車內的商品
+            cart.destroy().then(cart => {
+              return res.redirect(`/order/${order.id}`)
             })
           })
         })
